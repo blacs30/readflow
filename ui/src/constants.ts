95486251c82dda7643c16772cec87f064a319baf
Claas Lisowski <github@lisowski-development.com>
2022-01-07 00:58:52 +0100

    add space

--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -6,7 +6,7 @@ export const AUTHORITY = process.env.REACT_APP_AUTHORITY || 'https://gitlab.com'
 
 // OIDC client ID
 export const CLIENT_ID =
- process.env.REACT_APP_CLIENT_ID || 'f7e9fc0df823db99e29df61d9e883d7727258bcd089d927d9b3f43768defbbac'
+  process.env.REACT_APP_CLIENT_ID || 'f7e9fc0df823db99e29df61d9e883d7727258bcd089d927d9b3f43768defbbac'
 
 // Unauthenticated user redirect
 export const REDIRECT_URL = process.env.REACT_APP_REDIRECT_URL || '/login'
