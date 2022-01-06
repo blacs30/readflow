7c6ff3a30b130dfece2e010cf50e76822c843cc9
Claas Lisowski <github@lisowski-development.com>
2022-01-07 00:21:01 +0100

    update ui constants

--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -2,10 +2,10 @@
 export const API_BASE_URL = process.env.REACT_APP_API_ROOT || 'https://readflow-api.lisowski-development.com'
 
 // OIDC authority URL
-export const AUTHORITY = process.env.REACT_APP_AUTHORITY || 'mock'
+export const AUTHORITY = process.env.REACT_APP_AUTHORITY || 'https://gitlab.com'
 
 // OIDC client ID
-export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || 'webapp'
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || 'f7e9fc0df823db99e29df61d9e883d7727258bcd089d927d9b3f43768defbbac'
 
 // Unauthenticated user redirect
 export const REDIRECT_URL = process.env.REACT_APP_REDIRECT_URL || '/login'
