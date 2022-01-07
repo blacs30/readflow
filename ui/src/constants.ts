404f347424aef7feefcc4941640e9c6b9ca1c9b3
Claas Lisowski <github@lisowski-development.com>
2022-01-07 14:28:33 +0100

    use mock

--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -2,11 +2,11 @@
 export const API_BASE_URL = process.env.REACT_APP_API_ROOT || 'https://readflow-api.lisowski-development.de'
 
 // OIDC authority URL
-export const AUTHORITY = process.env.REACT_APP_AUTHORITY || 'https://gitlab.com'
+export const AUTHORITY = process.env.REACT_APP_AUTHORITY || 'mock'
 
 // OIDC client ID
 export const CLIENT_ID =
-  process.env.REACT_APP_CLIENT_ID || 'f7e9fc0df823db99e29df61d9e883d7727258bcd089d927d9b3f43768defbbac'
+  process.env.REACT_APP_CLIENT_ID || 'webapp'
 
 // Unauthenticated user redirect
 export const REDIRECT_URL = process.env.REACT_APP_REDIRECT_URL || '/login'
